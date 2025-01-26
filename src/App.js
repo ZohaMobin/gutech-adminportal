81adf07e272ed2403767956c7ab887ecf2fcf16e
Waqas <125983247+MuhammadWaqas3@users.noreply.github.com>
2025-01-26 16:16:26 +0500

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Sidebar from "./pages/Sidebar/sidebar/index";
-import Assignments from "./pages/assignments/assignments";
-import Dashboard from './pages/dashboard/dashboard';
-import { Topbar } from "./pages/topbar/topbar";
+import Sidebar from "../src/Pages/Sidebar/sidebar/index";
+import Assignments from './Pages/assignments/assignments';
 
+import Dashboard from './Pages/dashboard/dashboard';
+import { Topbar } from './Pages/topbar/topbar';
 
 function App() {
   return (
@@ -12,21 +12,22 @@ function App() {
       <div className="App">
         <Sidebar />
         <div className='mainContent '>
-          <Topbar />
-          <div className="routes-content" >
-          <Routes >
-  
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/assignments" element={<Assignments/>} />
+  <Topbar/>
+  <div className='content'>
+        <Routes >
 
+          <Route path="/" element={<Dashboard/>} /> 
+          <Route path="/assignments" element={<Assignments />} />
+      
         </Routes>
         </div>
+        </div>
       </div>
-    </div>
-    </Router >
+    </Router>
   );
 }
 
 export default App;
+
 
 
