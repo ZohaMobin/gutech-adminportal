a980d9b3a2d3d955607be46add36b06dfa46ba97
Muhammad Waqas3 <muhammad.waqas.56964@gmail.com>
2025-01-26 13:53:06 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Sidebar from "../src/pages/Sidebar/sidebar/index";
-import Assignments from './pages/assignments/assignments';
-
+import Sidebar from "./pages/Sidebar/sidebar/index";
+import Assignments from "./pages/assignments/assignments";
 import Dashboard from './pages/dashboard/dashboard';
-import { Topbar } from './pages/topbar/topbar';
+import { Topbar } from "./pages/topbar/topbar";
+import AnnouncementsPage from './pages/Announcement';
 
 function App() {
   return (
@@ -12,20 +12,20 @@ function App() {
       <div className="App">
         <Sidebar />
         <div className='mainContent '>
-  <Topbar/>
-        <Routes>
+          <Topbar />
+          <div className="routes-content" >
+          <Routes >
+          <Route path='/Announcement' element={<AnnouncementsPage />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/assignments" element={<Assignments/>} />
 
-          <Route path="/" element={<Dashboard/>} /> 
-          <Route path="/assignments" element={<Assignments />} />
-      
         </Routes>
         </div>
       </div>
-    </Router>
+    </div>
+    </Router >
   );
 }
 
 export default App;
-
-
 
