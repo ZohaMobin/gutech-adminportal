1ae179a9f8f5328e488d060c76c5946980c2fb66
Nightfury4210 <Ibrahimshahbaz4210@gmail.com>
2025-01-29 21:16:08 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from "../src/Pages/Sidebar/sidebar/index";
 import Assignments from './Pages/assignments/assignments';
 import AdminEventCalendar from './Pages/Event Calendar/Event Calendar';
-
+import Attendance from './Pages/Attendance/Attendance';
 import Dashboard from './Pages/dashboard/dashboard';
 import { Topbar } from './Pages/topbar/topbar';
 
@@ -21,6 +21,7 @@ function App() {
           <Route path="/" element={<Dashboard/>} /> 
           <Route path="/assignments" element={<Assignments />} />
           <Route path="/calendar" element={<AdminEventCalendar />}></Route>
+          <Route path="/Attendance" element={<Attendance />} />
       
         </Routes>
         </div>
