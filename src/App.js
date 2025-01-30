8f4f4972fffb69bc82ee2909a6227940d7e30842
Muhammad-Anas-Sid <siddiquimuhammdanas5@gmail.com>
2025-01-30 19:09:20 +0500

    Job Opportunities merged

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from "../src/Pages/Sidebar/sidebar/index";
 import Assignments from './Pages/assignments/assignments';
 import AdminEventCalendar from './Pages/Event Calendar/Event Calendar';
-
+import AdminJobsPage from "./Pages/JobsAndBootcamps/JobsAndBootcamps";
 import Dashboard from './Pages/dashboard/dashboard';
 import { Topbar } from './Pages/topbar/topbar';
 
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AdminJobsPage from "./Pages/JobsAndBootcamps/JobsAndBootcamps";
 function App() {
   return (
     <Router>
